99173d6a9ad4a48884d84f1ebc16b142c8404a75
APPLE4869 <apple.4869.s@gmail.com>
2020-09-08 14:36:16 +0900

    initialize spring boot

---
+++ src/test/kotlin/com/dddsample/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dddsample.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
